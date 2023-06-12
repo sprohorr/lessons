4d2dc33414fe10658cc372576ebfaa29e13ad4b2
sprohorr <136226347+sprohorr@users.noreply.github.com>
2023-06-12 22:42:18 +0300

    initial commit

---
+++ src/Lesson1/BooleanClass.java
@@ -0,0 +1,12 @@
+package Lesson1;
+
+import java.util.Scanner;
+public class BooleanClass {
+    public static void main(String[] args) {
+        boolean greater;
+        Scanner scanner = new Scanner(System.in);
+        int a  = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a);
+    }
+}
