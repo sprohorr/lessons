9b542176938fcbcfa55a87e3c42379a3e3efd315
sprohorr <sprohorr@gmail.com>
2023-06-15 22:16:07 +0300

    add homework2 fix

--- src/homework2/discount.java
+++ src/homework2/discount.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class discount {
     public static void main(String[] args) {
         double maxPrice = 1400;
-        double discount = maxPrice - (maxPrice * 0.07);
+
 
         System.out.println("Enter price:  ");
         Scanner scanner = new Scanner(System.in);
         double price = scanner.nextDouble();
-
+        double discount = price - (price * 0.07);
 
         double discountPrice = price >= maxPrice ? discount : price;
         System.out.println("Price: " + discountPrice);
