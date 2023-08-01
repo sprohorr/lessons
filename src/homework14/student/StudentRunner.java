33f9114fc4c2dfab04a284c1dc12b42183191f6a
sprohorr <sprohorr@gmail.com>
2023-08-01 21:19:19 +0300

    fix homework14

--- src/homework14/student/StudentRunner.java
+++ src/homework14/student/StudentRunner.java
@@ -2,19 +2,26 @@ package homework14.student;
 
 import homework14.student.model.Student;
 
-import static homework14.student.model.StudentCourseService.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static homework14.student.StudentCourseService.printStudents;
+import static homework14.student.StudentCourseService.upgradeCourse;
 
 public class StudentRunner {
 
     public static void main(String[] args) {
 
         Student student1 = new Student("Ivanov V.V.", 1, 1, 2);
-        Student student2 = new Student("Petrov P.P.", 1, 1, 2);
+        Student student2 = new Student("Petrov P.P.", 1, 1, 3);
         Student student3 = new Student("Sidorov S.S.", 1, 1, 3);
         Student student4 = new Student("Sokolov S.S.", 1, 1, 5);
-        students.add(String.valueOf(student1));
+        List<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
         upgradeCourse(students);
-        students.add(String.valueOf(student3));
         upgradeCourse(students);
         printStudents(students, 2);
     }
